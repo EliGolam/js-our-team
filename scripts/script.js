cc889157bb4d8197c7dfc49f4c4733306a84c9e4
EliGolam <elistasibusiness@gmail.com>
2022-09-07 01:56:53 +0200

    v0.9: Milestone 1,2 and Bonus 1,2 Completed

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+// Initial Handshake
+console.log('DEBUG - script.js: OK!');
+
+// 2d Array of Team Members Data
+const teamMembersData = [
+    ['Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'],
+    ['Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg'],
+    ['Walter Gordon', 'Office Manager', 'walter-gordon-office-manager.jpg'],
+    ['Angela Lopez', 'Social Media Manager', 'angela-lopez-social-media-manager.jpg'],
+    ['Scott Estrada', 'Developer', 'scott-estrada-developer.jpg'],
+    ['Barbara Ramos', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg']
+]
+
+// Array containing the Team Members
+const team = [];
+
+
+// For loop to create a TeamMember object for each 
+// team member and add them to the team array
+for(let member of teamMembersData) {
+    team.push(new TeamMember(...member));
+}
+
+
+
+/* ****************************************************************** */
+// Milestone 1: 
+// Print on console the information of each team member
+for(let member of team) {
+    let person = `
+        Team Member: ${member.name}
+            Position: ${member.position}
+            Photo: ${member.photo} 
+    `
+    console.log(person);
+}
+
+
+
+/* ****************************************************************** */
+// Milestone 2: 
+// Print the information on DOM
+
+// Initialize DOM elements
+const container = document.getElementById('test_team-members');
+
+
+for(let member of team) {
+    createCardDOM(member, container);
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,56 @@
+// Initial Handshake
+console.log('DEBUG - utilities.js: OK!');
+
+// Team Member Object constructor 
+class TeamMember {
+    constructor(name, position, photo) {
+        this.name = name;
+        this.position = position;
+        this.photo = photo;
+    }
+}
+
+
+// Function: Create card in DOM
+function createCardDOM(member, container) {
+    // Create a card for each member
+    const card = document.createElement('div');
+    card.classList.add('col-12', 'col-md-4'); 
+
+    // Create the container for the DOM elements
+    for (let property in member) {
+        switch (property) {
+            case 'name': {
+                card.prepend(addCardElement(member[property], 'h2'));
+                break;
+            }
+            case 'photo': {
+                card.append(addCardElement(member[property], 'img', member.name));
+                break;
+            }
+            default: {
+                card.append(addCardElement(member[property], 'p'));
+                break;
+            }
+        }
+    }
+
+    // Append the card to the container
+    container.append(card);
+}
+
+
+// Function: Add Elements in Card
+function addCardElement(content, elementType, imgAlt) {
+    const element = document.createElement(elementType);
+    if (elementType === 'img') {
+        // When it's an image we also add the img alt property for accessibility
+        element.classList.add('img-fluid');
+        element.src = `./assets/img/${content}`;
+        element.alt = `photo of ${imgAlt}`;
+    } else {
+        element.classList.add('text-center');
+        element.textContent = content;
+    }
+    return element;
+}
